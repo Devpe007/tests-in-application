d021eac7302bf16e2466934d3816a47d32d96d2e
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-07-27 14:36:39 -0300

    created middleware error

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,14 @@ import { routes } from "./routes/routes";
 
 import "dotenv/config";
 
+import { middlewareError } from "./middlewares/Error/Error";
+
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-      return response.status(400).json({
-        message: err.message,
-      });
-    }
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err}`,
-    });
-  }
-);
+app.use(middlewareError);
 
 export { app };
---
+++ src/middlewares/Error/Error.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+export function middlewareError(error: Error, request: Request, response: Response, next: NextFunction) {
+    if(error instanceof Error) {
+        return response.status(400).json({
+            message: error.message,
+        });
+    };
+
+    return response.status(500).json({
+        status: 'error',
+        message: `Internal server error = ${error}`
+    });
+};

fd40cfa8192790364de13bc3cb6493d55940360f
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-07-27 16:08:12 -0300

    validating if you are able to create a new user

--- src/modules/createUser/CreateUserController.spec.ts
+++ src/modules/createUser/CreateUserController.spec.ts
@@ -7,14 +7,15 @@ import request from 'supertest';
 
 describe("Create User Controller", () => {
     it("should be able to create a new user", async () => {
-        const response = await request(app)
-            .post('/users')
-            .send({
-                username: 'test-integration',
-                email: 'testIntegration@test.com.br',
-                name: 'Test Integration'
-            });
+        const response = await request(app).post('/users').send({
+            username: 'test-integration',
+            email: 'testIntegration@test.com.br',
+            name: 'Test Integration'
+        });
 
-        console.log(response.status);
+        console.log(response.body);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('id');
     });
 });

8cb7dc36a6194e0b5fd0f6484be4bdb7dff05aa5
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-07-27 16:02:46 -0300

    finalized prisma-environment-jest

---
+++ src/modules/createUser/CreateUserController.spec.ts
@@ -0,0 +1,20 @@
+/*
+    @jest-environment ./prisma/prisma-environment-jest
+*/
+
+import { app } from "../../app";
+import request from 'supertest';
+
+describe("Create User Controller", () => {
+    it("should be able to create a new user", async () => {
+        const response = await request(app)
+            .post('/users')
+            .send({
+                username: 'test-integration',
+                email: 'testIntegration@test.com.br',
+                name: 'Test Integration'
+            });
+
+        console.log(response.status);
+    });
+});
